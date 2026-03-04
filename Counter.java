7f9af634351237e084981bd082b93421e68913b6
hesse09 <157098954+hesse09@users.noreply.github.com>
2026-03-04 16:47:19 -0600

    Add Counter class to print numbers from 0 to 10

---
+++ Counter.java
@@ -0,0 +1,11 @@
+public class Counter {
+    public static void main(String[] args) {
+        int count = 1;
+        int counter = 0;
+        while (counter <= 10) {
+            System.out.println(counter);
+            counter = counter + count;
+        }
+
+    }
+}
